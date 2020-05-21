9b1421fad51847a0165e26634ca4a2e225542692
Nazario <nazariomp@hotmail.com>
2020-05-21 15:38:17 -0500

    se agrego la impresion

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { PrintService } from '../services/print.service';
+
+@Component({
+    selector:'app-home',
+    templateUrl:'home.page.html',
+    styleUrls:['home.page.scss'],
+})
+
+export class HomePage{
+    bluetoothList:any=[];
+    selectedPrinter:any;
+
+    constructor(private print:PrintService) {}
+    //This will list all of your bluetooth devices
+    listPrinter() { 
+      this.print.searchBluetoothPrinter()
+       .then(resp=>{
+   
+        //List of bluetooth device list
+        this.bluetoothList=resp;
+        });
+    }
+
+    //This will store selected bluetooth device mac address
+    selectPrinter(macAddress){
+        //Selected printer macAddress stored here
+        this.selectedPrinter=macAddress;
+    }
+
+    //This will print
+    printStuff(){  
+        //The text that you want to print
+        var myText="Hello hello hello \n\n\n This is a test \n\n\n";
+        this.print.sendToBluetoothPrinter(this.selectedPrinter,myText);
+    }
+
+}
+
